5414b7df3ce3e673e805fb78be91ca0ff886d73f
Alessio-Civitella <alessio.civitella19@gmail.com>
2025-06-12 12:49:23 +0200

    Risolto il bug per il quale non era possibile vedere in pagina i dettagli di ogni carne nella pagina di comparazione.

--- server.js
+++ server.js
@@ -300,9 +300,7 @@ const loadPromises = resourceTypes.map(type => {
             );
         }
         
-        res.json(filteredItems.map(
-            ({id, createdAt, updatedAt, title, category}) => ({id, createdAt, updatedAt, title, category})
-        ));
+        res.json(filteredItems);
     });
 
     // Load data for this resource type
